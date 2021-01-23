eff00311bfd655b2a4ccb6746637395ea658bdf8
Just <yurii.maidan@gmail.com>
2021-01-23 18:11:09 -0500

    Ivoke terraform execution via ansible

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
     backend "s3" {
         encrypt = "true"
-        bucket = "jentf-state-store"
+        bucket = "jentf-statefile-store"
         key = "terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "jenkins-tf"
